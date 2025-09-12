5cf823bf98d12112ef6223c7f2005cd9e8d8ac86
dkurniawan <dkurniawan@bglcorp.com.au>
2025-09-12 18:34:30 +1000

    Add login POST API

--- src/main/java/org/playground/shoppingcart/repositories/UserRepository.java
+++ src/main/java/org/playground/shoppingcart/repositories/UserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository  extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
 
     @Procedure("findLoyalUsers")
     List<UserSummary> findLoyalUsers(int loyaltyPoints);
+
+    Optional<User> findByEmail(String email);
 }
