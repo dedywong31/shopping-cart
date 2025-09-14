3149b5fbbe6d0f0e283ff8cbe918ce2403eed0dc
dkurniawan <dkurniawan@bglcorp.com.au>
2025-09-15 09:11:50 +1000

    Managing secrets

--- src/main/java/org/playground/shoppingcart/services/JwtService.java
+++ src/main/java/org/playground/shoppingcart/services/JwtService.java
@@ -1,14 +1,17 @@
 package org.playground.shoppingcart.services;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400;
 
@@ -16,7 +19,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 }
