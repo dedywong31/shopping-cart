44824271ae5f44df2983632f2eb0ec8c4ed70c92
dkurniawan <dkurniawan@bglcorp.com.au>
2025-09-26 12:17:16 +1000

    Add get current user endpoint

--- src/main/java/org/playground/shoppingcart/controller/AuthController.java
+++ src/main/java/org/playground/shoppingcart/controller/AuthController.java
@@ -4,12 +4,17 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.playground.shoppingcart.dtos.JwtResponse;
 import org.playground.shoppingcart.dtos.LoginRequest;
+import org.playground.shoppingcart.dtos.UserDto;
+import org.playground.shoppingcart.mappers.UserMapper;
+import org.playground.shoppingcart.repositories.UserRepository;
 import org.playground.shoppingcart.services.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -18,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -39,6 +46,21 @@ public class AuthController {
     public boolean validate(@RequestHeader("Authorization") String authHeader) {
         var token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
